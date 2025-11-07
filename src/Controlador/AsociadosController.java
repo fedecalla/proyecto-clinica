c6a5f0ada0c271771b760683c52a6e11f007a020
SalvadorM15 <salvadormunduteguy78@gmail.com>
2025-11-07 16:10:31 -0300

    comienzo del asociado controller

--- src/Controlador/AsociadosController.java
+++ src/Controlador/AsociadosController.java
@@ -1,7 +1,8 @@
 package Controlador;
 
-import Vista.VentanaSistema;
-import modelo.clinica.Clinica;
+import InterfazGrafica.VentanaAsociados;
+import Modelo.excepciones.AsociadoInvalidoException;
+import Modelo.clinica.Clinica;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,10 +14,10 @@ import java.util.ArrayList;
 public class AsociadosController implements ActionListener{
 	
 	
-	private VentanaSistema vista;
+	private VentanaAsociados vista;
 	private Clinica modelo;
 
-	public AsociadosController(VentanaSistema vista , Clinica modelo)
+	public AsociadosController(VentanaAsociados vista, Clinica modelo)
 	{
 		this.vista = vista;
 		this.modelo = modelo;
@@ -28,18 +29,15 @@ public class AsociadosController implements ActionListener{
 		
 		if(evento.getActionCommand().equalsIgnoreCase("AgregarAsociado"))
 		{
-			//Agrega un asociado a la base de datos y lo agrega al Arralist de Asociados
-			
-			//paso 1: crea un objeto asociado y chequea que todos los campos hayan sido correctamente completados
-				//si no se pudo crear tira alguna excepcion atrapada y que haga un popup informando el error
-			
-			//paso2:  llama a un metodo que lo agregue a la base de datos
-			//paso3: Agrega al objeto al arrayList de Asociados
-			
-			//paso4: llama a algun metodo de la vista que informa que la operacion fue exitosa
-			
-			
-			
+			String nombre, apellido, dni;
+			try {
+				this.vista.getDatosAsociado(nombre, apellido, dni);
+				this.modelo.nuevoAsociado(nombre, apellido, dni);
+				this.vista.popUp("asociado agregado con exito");
+			}
+			catch(AsociadoInvalidoException e) {
+				this.vista.popUp(e.getMessage());
+			}
 		}
 		else if(evento.getActionCommand().equalsIgnoreCase("EliminarAsociado"))
 		{
---
+++ src/Controlador/EventoMalLlamadoException.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+public class EventoMalLlamadoException extends Exception{
+	
+	public EventoMalLlamadoException(String mensaje)
+	{
+		super(mensaje);
+	}
+
+}
